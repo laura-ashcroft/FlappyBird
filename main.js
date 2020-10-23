b15554293e2be2b6e36ebb4e9691030c77e7e9b4
Laura Ashcroft <Laura@Helens-MBP.broadband>
2020-10-23 12:21:48 +0100

    obstacles ftw

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 window.onload = function() {
     const c = document.getElementById("canvas");
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
+    c.width = 706;
+    c.height = 530;
 
     const ctx = c.getContext("2d");
     
     const environment = new Environment(c, ctx);
     const bird = new Bird(50, 250, ctx);
-    const pipe1 = new Pipe (280, 350, 250, 2, ctx)
+    //const pipe1 = new Pipe (280, 350,  250, 2, ctx)
     const pipes = [];
 
     setInterval(function() {
         let pipeSet = generateRandomPipes(ctx, c.width, c.height);
-        pipes.push(pipeSet);
-    }, 1000)
+        pipes.push(pipeSet.top, pipeSet.bottom);
+    }, 3000);
 
+
+    ctx.fillStyle = "#FFFFFF";
     gameLoop();
 
 
@@ -22,30 +24,21 @@ window.onload = function() {
      ctx.fillRect(0,0, c.width, c.height);
      environment.update();
      environment.render();
-     /*pipes.forEach(function(pipe1){
+     pipes.forEach(function(pipe1){
         pipe1.update();
         pipe1.render(); 
-     });*/
-      pipe1.update();
-      pipe1.render();
-    
+     });
      bird.update();
      bird.render();
      window.requestAnimationFrame(gameLoop);
    }
  };
 
-/*function generateRandomPipes(ctx, canvasWidth, canvasHeight) {
-    let lengthTop = Math.round(Math.random()*200+100)
-    let lengthBottom = canvasHeight - 10 - lengthTop;
-    let returnVal = { };
-    returnVal.top = new Pipe(canvasWidth, -5, lengthTop, 3, ctx);
-    returnVal.bottom = new Pipe(canvasWidth, canvasHeight+5-lengthBottom, lengthBottom, 3, ctx);
-    return returnVal;
-}*/
-
-
 function generateRandomPipes(ctx, canvasWidth, canvasHeight) {
-    const pipe1 = new Pipe (280, 350, 250, 2, ctx)
-    return pipe1;
+    let lengthTop = Math.round(Math.random()*200+100);
+    let lengthBottom = 530 - 120 - lengthTop;
+    let returnVal = { };
+    returnVal.top = new Pipe(0, 706, lengthTop, 3, ctx);
+    returnVal.bottom = new Pipe(530+5-lengthTop, 706, lengthBottom, 3, ctx);
+    return returnVal;
 }
